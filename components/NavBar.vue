73a133e78ed2daeda388539d0f82e3025cf69575
Aarush Aggarwal <76877384+Aarushaggarwal-theone@users.noreply.github.com>
2023-06-08 23:38:03 +0530

    started workin

---
+++ components/NavBar.vue
@@ -0,0 +1,17 @@
+ 
+<template>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light" id="navpages">
+        <li>
+            <a class="nav-link" href="/">this is home</a>
+        </li>
+    </nav>
+    
+</template>
+<script lang="ts">
+
+
+export default {
+    name: 'Navbar'
+}
+
+</script> 
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,16 @@
 <template>
-  <Tutorial/>
+  <main>
+  <NavBar/>
+
+  </main>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import NavBar from '~/components/NavBar.vue';
 
 export default Vue.extend({
-  name: 'IndexPage'
+    name: "IndexPage",
+    components: { NavBar }
 })
 </script>
