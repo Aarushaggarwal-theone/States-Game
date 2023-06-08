334eb92c9168fb2896dfaad00708c6b7c05dad24
Aarush Aggarwal <76877384+Aarushaggarwal-theone@users.noreply.github.com>
2023-06-08 23:42:02 +0530

    initiated server requirements

---
+++ pages/api/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <main>
+        <NavBar />
+
+    </main>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import NavBar from '~/components/NavBar.vue';
+
+export default Vue.extend({
+    name: "IndexPage",
+    components: { NavBar }
+})
+</script>
